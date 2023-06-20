9619ee48415af7dfc4fb5d1d59cae565d1ae1020
Your Name <Your Email>
2023-06-20 22:26:44 +0530

    first commit

---
+++ build/precache-manifest.31bd8caaa08843fdac5703d51b0b65c2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4298cf45b24b5bf90b9ca1c32a3174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "4ca94e0dd130ec0ecc93",
+    "url": "/static/js/main.4daf7fb1.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
